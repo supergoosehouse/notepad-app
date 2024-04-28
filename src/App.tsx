1f2d826bcbac2eecd65652461d938eec3b3926b0
Andrew <39188673+arari02@users.noreply.github.com>
2024-04-28 11:17:50 -0600

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import NoteGrid from "./components/NoteGrid";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<NoteGrid></NoteGrid>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/NoteGrid.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoteGrid = () => {
+	return <div>NoteGridMock</div>;
+};
+
+export default NoteGrid;
