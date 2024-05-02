912d2b9e7bfcfea2cc41183518533ab661ed9e6a
Andrew <39188673+arari02@users.noreply.github.com>
2024-05-02 00:39:44 -0600

    account-dropdown-menu-mockup

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -4,27 +4,36 @@ import React from "react";
 const BurgerMenu = () => {
 	return (
 		<>
-			<div className="dropdown">
-				<button
-					className="btn btn-secondary dropdown-toggle"
-					type="button"
-					id="dropdownMenuButton"
-					data-toggle="dropdown"
-					aria-haspopup="true"
-					aria-expanded="false"
-				>
-					Dropdown button
-				</button>
-				<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-					<a className="dropdown-item" href="#">
-						Action
-					</a>
-					<a className="dropdown-item" href="#">
-						Another action
-					</a>
-					<a className="dropdown-item" href="#">
-						Something else here
-					</a>
+			<div
+				className="dropdown"
+				style={{
+					float: "right",
+					marginRight: "15px",
+				}}
+			>
+				<div className="dropdown">
+					<button
+						className="btn btn-secondary dropdown-toggle"
+						type="button"
+						id="dropdownMenuButton"
+						data-bs-toggle="dropdown"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false"
+					>
+						Dropdown button
+					</button>
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<a className="dropdown-item" href="#">
+							Action
+						</a>
+						<a className="dropdown-item" href="#">
+							Another action
+						</a>
+						<a className="dropdown-item" href="#">
+							Something else here
+						</a>
+					</div>
 				</div>
 			</div>
 		</>
